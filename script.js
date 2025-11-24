aa22a9a5fecc58f28b4a7ce53e1c64cb8955524e
BIKESHOPROMEO <tama@realeyes.co.jp>
2025-11-24 18:11:59 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -328,9 +328,13 @@ for (let i = 1; i <= blanks; i++) {
   console.timeEnd("描写開始");
   
   if (shoudScroll){
+    const today = new Date();
+    const isCurrentMonth = date.getFullYear() === today.getFullYear() && date.getMonth() === today.getMonth();
+    const offset = 100;
+
+    if (isCurrentMonth) {
     const todayCell = document.querySelector('.calendar-cell.today-highlight');
     if (todayCell) {
-      const offset = 100;
       const targetPosition = todayCell.getBoundingClientRect().top + window.scrollY - offset;
       window.scrollTo({
         top: targetPosition,
@@ -346,4 +350,4 @@ for (let i = 1; i <= blanks; i++) {
   });
 }
 }
-
+}
